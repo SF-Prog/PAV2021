db3974a1ee0e38378d6f72d35c01c0f0aef8d998
AgusPressel <40279471+AgusPressel@users.noreply.github.com>
2021-06-14 03:57:18 -0300

    rompe
    roompe

--- clases/Fabrica.cpp
+++ clases/Fabrica.cpp
@@ -1,19 +1,7 @@
 #include "../clases/Fabrica.h"
 
-#include "../controladores/ControladorAsignarAsignaturaDocente.h"
-#include "../controladores/ControladorAsistenciaClaseEnVivo.h"
-#include "../controladores/ControladorEliminarAsignatura.h"
-#include "../controladores/ControladorEnvioDeMensaje.h"
-#include "../controladores/ControladorFinalizacionDeAsistenciaClaseEnVivo.h"
-#include "../controladores/ControladorFinalizacionDeClase.h"
-#include "../controladores/ControladorInicioDeClase.h"
-#include "../controladores/ControladorInscripcionAsignatura.h"
-#include "../controladores/ControladorListarClases.h"
-#include "../controladores/ControladorReproduccionDeClaseEnDiferido.h"
-#include "../controladores/ControladorTiempoAsistenciaAClase.h"
-#include "../controladores/ControladorTiempoDictadoDeClases.h"
-#
-
+#include "../controladores/ControladorAltaAsignatura.h"
+#include "../controladores/ControladorAltaUsuario.h"
 #include <string>
 
 
@@ -30,9 +18,6 @@ Fabrica* Fabrica::getInstancia(){
     return instancia;
 }
 
-IControladorAClaseEnVivo* Fabrica::getIControladorAClaseEnVivo(){
-    return new ControladorAClaseEnVivo();
-}
 
 IControladorAltaAsignatura* Fabrica::getIControladorAltaAsignatura(){
     return new ControladorAltaAsignatura();
@@ -42,40 +27,5 @@ IControladorAltaUsuario* Fabrica::getIControladorAltaUsuario(){
     return new ControladorAltaUsuario();
 }
 
-IControladorAsignarAsignaturaDocente* Fabrica::getIControladorAsignarAsignaturaDocente(){
-    return new ControladorAsignarAsignaturaDocente();
-}
-
-IControladorAsignaturaDocente* Fabrica::getIControladorAsignaturaDocente(){
-    return new ControladorAsignaturaDocente();
-}
-
-IControladorAsistenciaClaseEnVivo* Fabrica::getIControladorAsistenciaClaseEnVivo(){
-    return new ControladorAsistenciaClaseEnVivo();
-}
-
-IControladorEliminarAsignatura* Fabrica::getIControladorEliminarAsignatura(){
-    return new ControladorEliminarAsignatura();
-}
-
-
-IControladorEnvioDeMensaje* Fabrica::getIControladorEnvioDeMensaje(){
-    return new ControladorEnvioDeMensaje();
-}
-
-
-IControladorInicioDeClase* Fabrica::getIControladorInicioDeClase(){
-    return new ControladorInicioDeClase();
-}
-
-
-IControladorInscripcionAsignatura* Fabrica::getIControladorInscripcionAsignatura(){
-    return new ControladorInscripcionAsignatura();
-}
-
-
-IControladorListarClases* Fabrica::getIControladorListarClases(){
-    return new ControladorListarClases();
-}
 
 Fabrica::~Fabrica(){}
--- clases/Fabrica.h
+++ clases/Fabrica.h
@@ -1,36 +1,14 @@
 #ifndef FABRICA
 #define FABRICA
 
-#include "../Interfaces/IControladorAClaseEnVivo.h"
-#include "../Interfaces/IControladorAltaAsignatura.h"
-#include "../Interfaces/IControladorAltaUsuario.h"
-#include "../Interfaces/IControladorAsignarAsignaturaDocente.h"
-#include "../Interfaces/IControladorAsignaturaDocente.h"
-#include "../Interfaces/IControladorAsistenciaClaseEnVivo.h"
-#include "../Interfaces/IControladorEliminarAsignatura.h"
-#include "../Interfaces/IControladorEnvioDeMensaje.h"
-#include "../Interfaces/IControladorInicioDeClase.h"
-#include "../Interfaces/IControladorInscripcionAsignatura.h"
-#include "../Interfaces/IControladorListarClases.h"
-
-
 class Fabrica{
     private: 
         static Fabrica* instancia;
         Fabrica();
     public:
         static Fabrica* getInstancia();
-        IControladorAClaseEnVivo* getIControladorAClaseEnVivo();
         IControladorAltaAsignatura* getIControladorAltaAsignatura();
         IControladorAltaUsuario* getIControladorAltaUsuario();
-        IControladorAsignarAsignaturaDocente* getIControladorAsignarAsignaturaDocente();
-        IControladorAsignaturaDocente* getIControladorAsignaturaDocente();
-        IControladorAsistenciaClaseEnVivo* getIControladorAsistenciaClaseEnVivo();
-        IControladorEliminarAsignatura* getIControladorEliminarAsignatura();
-        IControladorEnvioDeMensaje* getIControladorEnvioDeMensaje();
-        IControladorInicioDeClase* getIControladorInicioDeClase();      
-        IControladorInscripcionAsignatura* getIControladorInscripcionAsignatura();      
-        IControladorListarClases* getIControladorListarClases();        
         ~Fabrica();
 
 
